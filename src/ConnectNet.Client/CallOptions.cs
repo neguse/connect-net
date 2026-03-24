d57a7481bca52e8e4124ebc839a207f17789135b
neguse <neguse@neguse.dev>
2026-03-24 17:24:58 +0900

    feat: add ConnectContext and CallOptions

---
+++ src/ConnectNet.Client/CallOptions.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+
+namespace ConnectNet.Client;
+
+public class CallOptions
+{
+    public IDictionary<string, string> Headers { get; set; } = new Dictionary<string, string>();
+    public TimeSpan? Timeout { get; set; }
+    public IDictionary<string, string> ResponseTrailers { get; } = new Dictionary<string, string>();
+}
---
+++ src/ConnectNet/ConnectContext.cs
@@ -0,0 +1,22 @@
+using System.Collections.Generic;
+using System.Threading;
+
+namespace ConnectNet;
+
+public class ConnectContext
+{
+    public IDictionary<string, string> RequestHeaders { get; }
+    public IDictionary<string, string> ResponseHeaders { get; }
+    public IDictionary<string, string> ResponseTrailers { get; }
+    public CancellationToken CancellationToken { get; }
+
+    public ConnectContext(
+        IDictionary<string, string>? requestHeaders = null,
+        CancellationToken cancellationToken = default)
+    {
+        RequestHeaders = requestHeaders ?? new Dictionary<string, string>();
+        ResponseHeaders = new Dictionary<string, string>();
+        ResponseTrailers = new Dictionary<string, string>();
+        CancellationToken = cancellationToken;
+    }
+}
